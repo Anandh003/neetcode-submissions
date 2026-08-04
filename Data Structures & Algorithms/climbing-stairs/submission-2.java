a9f4ce50920c9420ade459dbd18162ceb0706c18
Anga <34986641+Anandh003@users.noreply.github.com>
2026-08-04 09:35:33 +0530

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.java
@@ -0,0 +1,26 @@
+class Solution {
+    int[] cache;
+
+    public int climbStairs(int n) {
+        cache = new int[n];
+        for (int i = 0; i < n; i++) {
+            cache[i] = -1;
+        }
+        return dfs(0, n);
+    }
+
+// Recursion + Memoization
+    public int dfs(int i, int target) {
+        if (i == target)
+            return 1;
+
+        if (i > target)
+            return 0;
+
+        if (cache[i] != -1)
+            return cache[i];
+
+        cache[i] = dfs(i + 1, target) + dfs(i + 2, target);
+        return cache[i];
+    }
+}
